b6837d27f74051f8b9a6f91d9c1170c9a34fd808
mohtar25 <mokhtar2000naj@gmail.com>
2024-03-02 13:44:22 +0100

    done with v1

---
+++ newrps.js
@@ -0,0 +1,6 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const butt = document.querySelector('button')
+    butt.addEventListener('click', console.log('ass'));
+});
--- rps.js
+++ rps.js
@@ -1,20 +1,14 @@
-function play()
+let score_cs = 0;
+let score_hu = 0;    
+function play(choice)
     {  
-        let answer;
-        while (true){
-        answer = prompt("Choose (rock, paper, scissors): ").toLowerCase();
-        answer = ["rock", "paper", "scissors"].indexOf(answer);
-        if (answer > -1){
-            break;
-        }
-        alert("Please Chose a correct item");
-        }
+        let answer = choice;
         let guess = Math.floor(Math.random() * 3);
         if (guess == answer){
-            console.log("Draw!")
+            print("Draw!")
             return;
         }
-        console.log(winner(answer, guess))
+        print(winner(answer, guess))
     }
     function cap(string){
         return string.at(0).toUpperCase() + string.slice(1);
@@ -24,16 +18,75 @@ function play()
         array = [[0, 0, 1], [1, 0, 0], [0, 1, 0]];
         items = ["rock", "paper", "scissors"]
         if (array[answer][guess] > array[guess][answer]){
+            score_hu++;
             return "You win!!";
+
         }
         else{
+            score_cs++;
             return `You lost!, ${cap(items[guess])} beats ${cap(items[answer])}`
+            
         }
     }
-    let i = 0;
-    while (i < 5)
-    {
-        i++;    
-        play(); 
-        console.log(`Round ${i} out of 5`)
+
+    function run(type){
+
+
+        if (score_cs < 5  && score_hu < 5){
+            play(type);
+            update();
+        }
+        else{
+        if (score_cs >= 5){
+            print("You lost The Game!, Better luck next time!, Click any button to restart")
+        
+        }  
+        if (score_hu >= 5){
+        print("You Won The Game!, Click any button to restart")
+        
+        }
+        
+        score_cs =0;
+        score_hu= 0;
+
     }
+
+       
+        
+    }
+    
+    const rock = document.querySelector('.rock');
+    rock.addEventListener("click", (e)=>{
+        run(rock.value);
+    }    )
+
+    const paper = document.querySelector('.paper');
+    paper.addEventListener("click", (e)=>{
+        run(paper.value);
+    }    )
+
+    const scissors = document.querySelector('.scissors');
+    scissors.addEventListener("click", (e)=>{
+        run(scissors.value);
+    }    )
+
+        function update(){
+        document.querySelector(".human").textContent = `Your Score is ${score_hu}`;
+        document.querySelector(".comp").textContent = `Computer's score is ${score_cs}`;
+        }
+        update()
+
+        function print(text){
+            document.querySelector('h2').textContent = text;
+        }
+        
+
+
+
+
+
+
+
+
+
+
